0604d08df74007360d2644a181d00e03b86ee05a
Naman Chaubey <namanchaubey31@gmail.com>
2025-08-02 22:34:19 +0530

    codesubmission.js

--- server/index.js
+++ server/index.js
@@ -19,4 +19,4 @@ mongoose.connect(process.env.MONGO_URI)
     console.log('MongoDB connected');
     app.listen(5000, () => console.log('Server running on port 5000'));
   })
-  .catch(err => console.error(err));////
+  .catch(err => console.error(err));/////
